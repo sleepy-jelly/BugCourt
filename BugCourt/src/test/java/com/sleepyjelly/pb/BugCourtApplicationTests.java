eabd8de87431fb4c3325fbe8861b06238210ea20
sleepy-jelly <kernel2015@naver.com>
2025-01-31 14:01:05 +0900

    test: Issue#31 github action build error

--- BugCourt/src/test/java/com/sleepyjelly/pb/BugCourtApplicationTests.java
+++ BugCourt/src/test/java/com/sleepyjelly/pb/BugCourtApplicationTests.java
@@ -2,8 +2,9 @@ package com.sleepyjelly.pb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(classes = BugCourtApplicationTestConfig.class, webEnvironment = WebEnvironment.DEFINED_PORT)
 class BugCourtApplicationTests {
 
 	@Test
