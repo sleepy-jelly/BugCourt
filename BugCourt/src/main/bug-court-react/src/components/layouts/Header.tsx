44d9aacd0f0d0fc2fc45e97fae0bf9ac99b755f9
sleepy-jelly <kernel2015@naver.com>
2025-02-03 14:20:22 +0900

    refactoring : changed some of imgs directory src - > public
    favicon, logo, vite, changed directory
    
    fixed Header.

--- BugCourt/src/main/bug-court-react/src/components/layouts/Header.tsx
+++ BugCourt/src/main/bug-court-react/src/components/layouts/Header.tsx
@@ -4,8 +4,14 @@ import 'simple-datatables';
 const Header = () => {
   return(
     <>
+      {/* Google Fonts */}
+      <link href="https://fonts.gstatic.com" rel="preconnect" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet"
+      />
       <header>Header Component.</header>    
-      <>
+        <>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
@@ -13,12 +19,11 @@ const Header = () => {
             name="viewport"
             content="width=device-width, maximum-scale=1.0, minimum-scale=1, user-scalable=yes,initial-scale=1.0"
           />
+       
           <title>BugCourt</title>
           {/* Template Main CSS File */}
           <link type="text/css" rel="stylesheet" href="../../assets/css/style.css" />
           {/* Vendor JS Files */}
-          {/* Template Main JS File */}
-          {/* Vendor CSS FilesC */}
           <link
             type="text/css"
             href="../../assets/vendor/bootstrap/css/bootstrap.min.css"
@@ -54,15 +59,9 @@ const Header = () => {
             href="../../assets/vendor/simple-datatables/style.css"
             rel="stylesheet"
           />
-          {/* Favicons */}
-          <link rel="shortcut icon" href="/img/favicon.png" />
-          <link rel="shortcut icon" href="/img/favicon.png" />
-          {/* Google Fonts */}
-          <link href="https://fonts.gstatic.com" rel="preconnect" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-            rel="stylesheet"
-          />
+          {/* Template Main JS File */}
+          {/* Vendor CSS FilesC */}
+        
         </>
     </>
   )
