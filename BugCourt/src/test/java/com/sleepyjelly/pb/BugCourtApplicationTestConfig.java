891d355d04b0ff5702c25fd48446ab02a630333c
sleepy-jelly <kernel2015@naver.com>
2025-02-07 11:26:27 +0900

    bug fix : exclude = {DataSourceAutoConfiguration.class } in test

--- BugCourt/src/test/java/com/sleepyjelly/pb/BugCourtApplicationTestConfig.java
+++ BugCourt/src/test/java/com/sleepyjelly/pb/BugCourtApplicationTestConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan(nameGenerator = CustomBeanNameGenerator.class)
 public class BugCourtApplicationTestConfig {
 
